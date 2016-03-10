4c09e6e7b434d96758fe4fafa02d4dd62150cfbb
zanjelen <zan.jelen@icloud.com>
2016-03-10 09:38:38 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -19,8 +19,8 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		
-		document.querySelector("#uporabnik").innerHTML += " \
-			<div class ='opomnik'> \
+		document.querySelector("#opomniki").innerHTML += " \
+			<div class ='opomnik senca rob'> \
 				<div class ='naziv_opomnika'>" + naziv_opomnika + "</div> \
 				<div class='cas_opomnika'> Opomnik čez <span>"+cas_opomnika+"</span> sekund.</div> \
 			</div>";
@@ -43,6 +43,19 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			
+			if(cas == 0)
+			{
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				
+				alert("Opomnik!\n\nZadolžitev '"+ naziv_opomnika + "' je potekla!");
+				
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else
+			{
+				casovnik.innerHTML = cas - 1;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
